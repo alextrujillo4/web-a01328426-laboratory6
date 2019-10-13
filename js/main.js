6304b62cd280520d0eb8a13b2bba0e704c43ef15
Alex Trujillo <a01328426@itesm.mx>
2019-10-12 23:04:25 -0500

    youtube lab done

--- js/main.js
+++ js/main.js
@@ -1,42 +1,103 @@
-var i = 0;
-function addEventListener(){
-    $('#shop_input').val("");
-        $('#button_submit').on('click', function (event){
-      
-            event.preventDefault();
-            let item = $('#shop_input').val()
-            if(item != "" ){
-                $('#shop_list').append(`<li class="list_item item_${i}">
-                    <p class="list_body">${item}</p> 
-                    <div class="item_buttons">
-                        <button class="btn_check" >Check</button>
-                        <button class="btn_delete">Delete</button>
+let url = "https://www.googleapis.com/youtube/v3/search";
+let apiKey = 'AIzaSyAea5vw_5utzmze7O2i5xEn4MeSXbLwI8g';
+let LASTSEARCH = null;
+let NEXTPAGE = null;
+function addData(responseJSON) {
+    responseJSON.items.forEach(function(video) {
+        console.log(video);
+        let videoUrl = 'https://www.youtube.com/watch?v=' + video.id.videoId;
+        $('#search-container').append(`
+                <div  class="card horizontal white">
+                    <div class="card-image">
+                      <a href="${videoUrl}" target="_blank">
+                      <img class="materialboxed" width="200" src="${video.snippet.thumbnails.medium.url}">
+                        </a>
                     </div>
-                </li>`);
-                $('#shop_input').val("");
-                i++;
+                    <div class="card-stacked">
+                      <div class="card-content">
+                        <div class="subtitle">${video.snippet.channelTitle}</div>
+                        <b>${video.snippet.title}</b>
+                        <p>${video.snippet.description}</p>
+                      </div>
+                      <div class="card-action">
+                        <a href="${videoUrl}" target="_blank">Abrir video en nueva página</a>
+                      </div>
+                    </div>
+                  </div>
+                `);
+    });
+    $('#no-content').hide();
+    $('#buscar-mas').show();
+}
+function callYoutubeAPI(searchdata) {
+    LASTSEARCH = searchdata;
+    $.ajax({
+        url: url,
+        data: {
+            part: "snippet",
+            q: searchdata,
+            key: apiKey,
+            maxResults: 10,
+            type: "video"
+        },
+        method: "GET",
+        dataType: "json",
+        success: function (responseJSON) {
+            addData(responseJSON)
+            $('#search-input').val("");
+            NEXTPAGE = responseJSON.nextPageToken;
+        },
+        error: function (err) {
+            M.toast({html: 'Error de llamada a la API', classes: 'rounded'});
+        }
+    });
+}
+function callMoreYoutubeAPI() {
+    $.ajax({
+        url: url,
+        data: {
+            part: "snippet",
+            q: LASTSEARCH,
+            key: apiKey,
+            maxResults: 10,
+            type: "video",
+            pageToken: NEXTPAGE
+        },
+        method: "GET",
+        dataType: "json",
+        success: function (responseJSON) {
+            addData(responseJSON)
+            $('#search-input').val("");
+            NEXTPAGE = responseJSON.nextPageToken;
+        },
+        error: function (err) {
+            M.toast({html: 'Error de llamada a la API', classes: 'rounded'});
+        }
+    });
+}
 
+function addEventListener(){
+    $('#search-input').val("");
+        $('#search-button').on('click', function (event){
+            event.preventDefault();
+            let searchitem = $('#search-input').val()
+            if(searchitem !== "" ){
+                callYoutubeAPI(searchitem)
             }else{
-                alert("Empty Item");
+                M.toast({html: 'La barra de búsqueda está vacía...', classes: 'rounded'});
             }
       });
 
-      
-} 
+}
+function addSearchMoreListener(){
+    $('#buscar-mas').on('click', function (event){
+        event.preventDefault();
+        callMoreYoutubeAPI()
+    });
+}
 
-$(document).on('click','.btn_check', function(event){
-    console.log("Click check")
-    event.preventDefault(); 
-    let listbody = $(this).parent().parent().find('.list_body');
-    if(listbody.hasClass('item_checked')){
-        listbody.removeClass('item_checked')
-    }else{
-        listbody.addClass('item_checked')
-    }
-});
 
-$(document).on('click','.btn_delete', function(event){
-    event.preventDefault();
-    $(this).parent().parent().remove();
-});
+$('#buscar-mas').hide();
 addEventListener();
+addSearchMoreListener();
+
